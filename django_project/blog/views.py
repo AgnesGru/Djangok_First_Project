6955a5b1edd8be068a2d1fd6a52d6f6cf1276467
Agnes Gruszecka <agnieszka.gruszecka@gmail.com>
2020-09-02 12:45:02 +0200

    some little changes like making our blog page the homepage

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,3 +7,5 @@ def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
 
 
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
